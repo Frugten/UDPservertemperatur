c467d86aef10b18bf24d71d278c5bc55a4b3c9b5
Frugten <75611171+Frugten@users.noreply.github.com>
2021-11-17 11:22:33 +0100

    Add project files.

---
+++ UDP server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using UdpTemperatur;
+
+namespace UDP_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UDP server!");
+
+            using (UdpClient socket = new UdpClient())
+            {
+                socket.Client.Bind(new IPEndPoint(IPAddress.Any, 7010));
+
+                while (true)
+                {
+                    SensorWork myworker = new SensorWork();
+                    IEnumerable<SensorTemperatur> items = myworker.GetAllItmesTask().Result;
+
+                    IPEndPoint clientEndpoint = null;
+                    byte[] received = socket.Receive(ref clientEndpoint);
+                    Console.WriteLine("Client connected: " + clientEndpoint.Address);
+                    Console.WriteLine("Temperatur: " + Encoding.UTF8.GetString(received));
+
+                    SensorTemperatur newItem = new SensorTemperatur(Encoding.UTF8.GetString(received), DateTime.Now);
+                    SensorTemperatur resultItem = myworker.PostItem(newItem).Result;
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ UDP server/SensorWork.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using UdpTemperatur;
+
+namespace UDP_server
+{
+    class SensorWork
+    {
+        public async Task<IEnumerable<SensorTemperatur>> GetAllItmesTask()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("https://sensordatarest.azurewebsites.net/api/Sensor");
+                IEnumerable<SensorTemperatur> Sensor = await response.Content.ReadFromJsonAsync<IEnumerable<SensorTemperatur>>();
+                return Sensor;
+            }
+        }
+
+        public async Task<SensorTemperatur> PostItem(SensorTemperatur sensor)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                JsonContent serialzedSensor = JsonContent.Create(sensor);
+                HttpResponseMessage response = await client.PostAsync("https://sensordatarest.azurewebsites.net/api/Sensor", serialzedSensor);
+                return await response.Content.ReadFromJsonAsync<SensorTemperatur>();
+            }
+        }
+    }
+}
